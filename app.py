ebb8cbf16588e0c2e42919abf5f26fb1f600c573
Daniel Sarpong <dansarpong.me@gmail.com>
2023-12-24 11:51:46 +0000

    Initial Update
    Add import for requests and update index route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    return render_template("index.html", title="Home")
